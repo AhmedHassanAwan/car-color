72cc83a72ea2d0de04623c59e230883208398c62
ahmedhassanawan <awanhasan2007@gmail.com>
2025-03-27 00:51:16 +0500

    color change car

--- app.js
+++ app.js
@@ -1,31 +1,24 @@
-// console.log("hellow world");
+const input = document.querySelector("#input");
+const h1 = document.querySelector("h1")
+const hellow = document.querySelector("#hellow");
+const image = document.querySelector("#image");
+const hexCode = document.querySelector("#hexCode");
 
 
 
+input.addEventListener('input', () => {
 
-// const input = document.querySelector("#input")
-// const hellow = document.querySelector("#hellow")
-// const btn = document.querySelector("#btn")
-// const image = document.querySelector("#image")
-
-
-
-
-
-
-// input.addEventListener( 'input' , ()=>{
-    
-    
-//     hellow.style.color = input.value
-//     image.style.color = input.value
+    h1.style.color = input.value;
+    hellow.style.color = input.value;
+    image.style.color = input.value;
+    hexCode.textContent = input.value.toUpperCase();
+    hexCode.style.color = input.value;
+});
 
 
     
-    
-  
-    
-    
-// })
 
-
-
+    image.style.transform = 'translateX(-50%) scale(1.1)';
+    setTimeout(() => {
+        image.style.transform = 'translateX(-50%) scale(1)';
+    }, 200);
