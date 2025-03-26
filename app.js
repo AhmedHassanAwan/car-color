99cc49b497c5d4d1267fdbb93587088694a8bf1a
ahmedhassanawan <awanhasan2007@gmail.com>
2025-03-27 00:26:15 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+// console.log("hellow world");
+
+
+
+
+// const input = document.querySelector("#input")
+// const hellow = document.querySelector("#hellow")
+// const btn = document.querySelector("#btn")
+// const image = document.querySelector("#image")
+
+
+
+
+
+
+// input.addEventListener( 'input' , ()=>{
+    
+    
+//     hellow.style.color = input.value
+//     image.style.color = input.value
+
+
+    
+    
+  
+    
+    
+// })
+
+
+
